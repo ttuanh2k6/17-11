d496de3c6d82e78a995a8bca43ed38cef54e2c5c
ttuanh2k6 <ttuanh2k6@gmail.com>
2025-11-17 15:04:15 +0700

    Add files via upload

---
+++ bt2.py
@@ -0,0 +1,23 @@
+# Vd1
+a = 2
+while a <= 10:
+    print(f'{a}', end= ' ')
+    a += 2
+# Vd2
+b = 3
+while b <= 18:
+    print(f'{b}', end=' ')
+    b += 3
+# Vd3
+c = 5
+while c <= 100:
+    print(f'{c}', end=' ')
+    c += 5
+# Vd4
+d = 1
+while d <= 100:
+    if d!=100:
+        print(f'{d}', end='+')
+    else:
+        print(f'{d}')
+    d += 1
